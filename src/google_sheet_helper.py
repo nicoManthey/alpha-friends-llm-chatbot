71bc46b57cd1b87a4fa79360df9da47b80cc9f4d
Nico Manthey <nico.manthey@big-picture.com>
2024-07-07 14:26:46 +0200

    Write number of completed questionnaires on G sheets

--- src/google_sheet_helper.py
+++ src/google_sheet_helper.py
@@ -8,8 +8,10 @@ from src.load_env_vars import G_SHEET_ID
 root_dir = Path(__file__).resolve().parents[1]
 credentials_path = root_dir / "google_sheets_credentials.json"
 
+
 class GSheetHelper:
     """A class to interact with Google Sheets API."""
+
     def __init__(self, sheet_id=G_SHEET_ID, credentials_path=credentials_path):
         self.sheet_id = sheet_id
         self.credentials_path = credentials_path
@@ -20,7 +22,9 @@ class GSheetHelper:
     def authorize(self):
         """Authorize the Google Sheets API client."""
         scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-        creds = Credentials.from_service_account_file(self.credentials_path, scopes=scopes)
+        creds = Credentials.from_service_account_file(
+            self.credentials_path, scopes=scopes
+        )
         self.client = gspread.authorize(creds)
         self.workbook = self.client.open_by_key(self.sheet_id)
 
@@ -37,7 +41,7 @@ class GSheetHelper:
 
         column_a_values = self.sheet.col_values(1)
         for i, value in enumerate(column_a_values):
-            if value == '':
+            if value == "":
                 return i + 1
         return len(column_a_values) + 1
 
@@ -45,6 +49,14 @@ class GSheetHelper:
         """Append data to the selected worksheet."""
         if not self.sheet:
             raise Exception("Sheet not selected. Call select_worksheet() first.")
-        start_cell = f'A{self._get_first_empty_row_in_column_a()}'
+        start_cell = f"A{self._get_first_empty_row_in_column_a()}"
         self.sheet.update(data, start_cell)
 
+    def get_number_of_completed_questionnaires(self):
+        """Return the number of completed questionnaires from G sheets."""
+        if not self.sheet:
+            raise Exception("Sheet not selected. Call select_worksheet() first.")
+        # Get all values from column A, starting from the second row
+        column_a_values = self.sheet.col_values(1)[1:]  # Start in cell A2
+        unique_dates = set(column_a_values)
+        return len(unique_dates)
